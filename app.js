113c1d9134132bd3d6d915119a40802badbe6f87
Thaddeus Stewart <thad@truqcapp.com>
2017-09-28 00:20:30 -0700

    add support pug local data, move old projects templates to json, restructure view directory to allow multiple static pages

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const stylus = require('stylus');
 
 const buildDir = path.join(__dirname, 'dist');
 const cssDir = path.join(__dirname, 'src', 'assets', 'css');
+const dataDir = path.join(__dirname, 'data');
+const htmlDir = path.join(__dirname, 'src', 'views', 'pages');
 
 function writeFile(filename, data) {
   fs.writeFile(path.join(buildDir, filename), data, (err) => {
@@ -12,12 +14,20 @@ function writeFile(filename, data) {
   });
 }
 
-
 // build the `dist` directory if it doesn't exist
 if (!fs.existsSync(buildDir)) fs.mkdirSync(buildDir);
 
 // tranform pug -> html
-writeFile('index.html', pug.renderFile(path.join(__dirname, 'src', 'views', 'index.pug')));
+const files = fs.readdirSync(htmlDir);
+files.forEach((filename) => {
+  const cleanName = filename.split('.')[0];
+  const dataFilePath = path.join(dataDir, `${cleanName}.json`);
+  const outputFilename = `${cleanName}.html`;
+  const locals = fs.existsSync(dataFilePath) ? require(dataFilePath) : undefined;
+  const html = pug.renderFile(path.join(htmlDir, filename), locals);
+
+  writeFile(outputFilename, html);
+});
 
 // transform stylus -> css
 stylus(fs.readFileSync(path.join(cssDir, 'master.styl'), { encoding: 'utf8' }))
@@ -25,6 +35,7 @@ stylus(fs.readFileSync(path.join(cssDir, 'master.styl'), { encoding: 'utf8' }))
   .include(path.join(__dirname, 'node_modules', 'iconfonts', 'stylesheets'))
   .render((err, css) => {
     if (err) throw err;
+
     writeFile('styles.css', css);
   });
 
